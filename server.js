65bb38af6f1522987a92dd271b5b62e72961e5c3
C4Q <c4q@C4Qs-MBP-75.fios-router.home>
2018-07-25 05:42:50 -0400

    depoly

--- server.js
+++ server.js
@@ -44,6 +44,16 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
+// const port = process.env.PORT || 5000;
+
+// app.listen(port, () => console.log(`Server running on port ${port}`));
+
+const host = 'localhost';
+const port = 5000;
+
+const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});
