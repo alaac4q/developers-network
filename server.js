eec756301f42edf759a4ab7323efe53f6142439c
C4Q <c4q@C4Qs-MBP-75.fios-router.home>
2018-08-27 20:19:50 -0400

    developers network

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
-const path = require('path');
-
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
@@ -34,32 +32,6 @@ app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
-// Server static assets if in production
-if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
+const port = process.env.PORT || 5000;
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, () => console.log(`Server running on port ${port}`));
-
-// const host = 'localhost';
-// const port = 5000;
-
-// const host = '0.0.0.0';
-// const port = process.env.PORT || 5000;
-
-// app.listen(port, host, function() {
-//   console.log("Server started.......");
-// });
-
-var server_port = process.env.PORT || process.env.PORT || 5000;
-var server_host = process.env.HOST || '0.0.0.0';
-app.listen(server_port, server_host, function() {
-    console.log('Listening on port %d', server_port);
-});
+app.listen(port, () => console.log(`Server running on port ${port}`));
